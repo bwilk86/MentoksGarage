4ec9da12d7e9c81cdee450b8883d54070a0d8760
Bryce <bwilkie21@hotmail.com>
2019-10-09 16:43:06 -0700

    working version on Raspi3 A+

--- venv/WebApi.py
+++ venv/WebApi.py
@@ -36,7 +36,7 @@ def door_task():
         relay_momentary_button(garage_door_relay_pin)
         time.sleep(20)
         state = sensor_read(garage_door_sensor_pin)
-        if (state):
+        if(state):
             return 'TRUE'
         else:
             return 'FALSE'
@@ -45,33 +45,33 @@ def door_task():
 def light_task():
     if(request.method == 'GET'):
         state = GPIO.input(garage_lights_relay_pin)
-        if (state):
+        if(state):
             return 'TRUE'
         else:
             return 'FALSE'
     else:
         state = relay_state_change(garage_lights_relay_pin)
-        if (state):
+        if(state):
             return 'TRUE'
         else:
             return 'FALSE'
 
 def sensor_read(pin):
-    GPIO.output(pin, GPIO.High)
+    GPIO.output(pin, True)
     state = GPIO.input(pin)
-    GPIO.output(pin, GPIO.Low)
+    GPIO.output(pin, False)
     return state
 
 def relay_state_change(pin):
     state = GPIO.input(pin)
     GPIO.output(pin, not state)
+    return GPIO.input(pin)
 
 def relay_momentary_button(pin):
+    GPIO.output(pin, False)
     state = GPIO.input(pin)
     GPIO.output(pin,not state)
     time.sleep(.2)
     GPIO.output(pin, state)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=8090)
+app.run(host='0.0.0.0', port=8090, debug=True)
