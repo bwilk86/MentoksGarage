e07c145d90bb6876273781fda50ceea1fd8514b8
Bryce <bwilkie21@hotmail.com>
2019-10-12 15:09:04 -0700

    Working on javascript to call webAPI

--- html/js/ButtonFunctions.js
+++ html/js/ButtonFunctions.js
@@ -12,10 +12,11 @@ function GarageDoor(act){
         $('#garageDoorOpen').removeAttr('disabled');
         }
     var apiUrl="http://10.199.248.169:8090/api/door/";
-    var data={'action': act};
+    var data={action: act};
     $.ajax({
     url: apiUrl,
     type: 'POST',
+    contentType: "application/json",
     dataType: 'json',
     data: data,
     success: function(d){
@@ -43,8 +44,7 @@ function GarageLights(act){
     type: 'POST',
     dataType: 'json',
     data: data,
-    success: function(d){
-    alert("This worked");},
+    success: function(d){alert("This worked");},
     error: function(){alert("This didn't work");}
-    });
+    });s
 }
